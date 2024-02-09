760e50d21fbb80aee53980c358581106607f57ae
YoNightFury <abhilashmaniyar70@gmail.com>
2024-02-10 02:58:53 +0530

    app2.go that comes under the same package as other files of the package.

---
+++ app2.go
@@ -0,0 +1,13 @@
+package greetings
+
+import "fmt"
+
+func Hello2(name string) string {
+	// longer way of declaring variables
+	// var message string
+	// message = fmt.Sprintf("Hello, %s", name)
+
+	// shortcut variable declaration and initialization
+	message := fmt.Sprintf("Hello, %s", name)
+	return message
+}
