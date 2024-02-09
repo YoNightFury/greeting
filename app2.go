f2ffbc3388bb40ad10c4cb7e29d62f8536683ebc
YoNightFury <abhilashmaniyar70@gmail.com>
2024-02-10 03:10:42 +0530

    Dry

--- app2.go
+++ app2.go
@@ -1,18 +1,11 @@
 package greetings
 
 import (
-	"fmt"
-
 	"rsc.io/quote"
 )
 
 func Hello2(name string) string {
-	// longer way of declaring variables
-	// var message string
-	// message = fmt.Sprintf("Hello, %s", name)
-
-	// shortcut variable declaration and initialization
-	message := fmt.Sprintf("Hello, %s", name)
+	message := Hello(name)
 	message = message + "\n" + quote.Go()
 	return message
 }
