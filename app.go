292ea04feb26b339aa88ed44d7eef2fda1cf9c81
YoNightFury <abhilashmaniyar70@gmail.com>
2024-02-10 11:07:22 +0530

    random

--- app.go
+++ app.go
@@ -1,13 +1,28 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
-func Hello(name string) string {
+func Hello(name string, format ...string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("name is mandatory")
+	}
+
 	// longer way of declaring variables
 	// var message string
 	// message = fmt.Sprintf("Hello, %s", name)
 
+	if len(format) > 0 {
+		if strings.Contains(format[0], "%s") {
+			return fmt.Sprintf(format[0], name), nil
+		}
+		return "", errors.New("the formated string should contain %s")
+	}
 	// shortcut variable declaration and initialization
 	message := fmt.Sprintf("Hello, %s", name)
-	return message
+	return message, nil
 }
--- app2.go
+++ app2.go
@@ -1,11 +1,19 @@
 package greetings
 
 import (
+	"math/rand"
+
 	"rsc.io/quote"
 )
 
-func Hello2(name string) string {
-	message := Hello(name)
+func Hello2(name string) (string, error) {
+	message, err := Hello(name)
 	message = message + "\n" + quote.Go()
-	return message
+	return message, err
+}
+
+func RandomHello(name string) (string, error) {
+	// var formats []string
+	formats := []string{"Hi, %s", "Howdy, %s", "Wrong un"}
+	return Hello(name, formats[rand.Intn(len(formats))])
 }
